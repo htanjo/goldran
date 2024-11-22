3f133e2b548c268662451689437b9398c51f8810
Hiroyuki Tanjo <expheno@gmail.com>
2024-11-22 23:08:06 +0900

    Updates

---
+++ assets/hdrFiltering.fragment-ioU4j9PZ.js
@@ -0,0 +1,5 @@
+import{S as e}from"./Screen-ByIAW_fE.js";import"./helperFunctions-Dr7h6LWL.js";import"./hdrFilteringFunctions-k5iu7J3i.js";import"./index-CDvpiCTI.js";const r="hdrFilteringPixelShader",i=`#include<helperFunctions>
+#include<importanceSampling>
+#include<pbrBRDFFunctions>
+#include<hdrFilteringFunctions>
+uniform float alphaG;uniform samplerCube inputTexture;uniform vec2 vFilteringInfo;uniform float hdrScale;varying vec3 direction;void main() {vec3 color=radiance(alphaG,inputTexture,direction,vFilteringInfo);gl_FragColor=vec4(color*hdrScale,1.0);}`;e.ShadersStore[r]=i;const a={name:r,shader:i};export{a as hdrFilteringPixelShader};
---
+++ assets/imageProcessing.fragment-Bgpp7-KO.js
@@ -0,0 +1,18 @@
+import{S as i}from"./Screen-ByIAW_fE.js";import"./imageProcessingFunctions-B5XrilXV.js";import"./helperFunctions-Dr7h6LWL.js";import"./index-CDvpiCTI.js";const e="imageProcessingPixelShader",r=`varying vec2 vUV;uniform sampler2D textureSampler;
+#include<imageProcessingDeclaration>
+#include<helperFunctions>
+#include<imageProcessingFunctions>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void)
+{vec4 result=texture2D(textureSampler,vUV);
+#ifdef IMAGEPROCESSING
+#ifndef FROMLINEARSPACE
+result.rgb=toLinearSpace(result.rgb);
+#endif
+result=applyImageProcessing(result);
+#else
+#ifdef FROMLINEARSPACE
+result=applyImageProcessing(result);
+#endif
+#endif
+gl_FragColor=result;}`;i.ShadersStore[e]=r;const a={name:e,shader:r};export{a as imageProcessingPixelShader};
