81496fad5bb9fc3f3f70ff2f7c380b1459e6907c
Hiroyuki Tanjo <expheno@gmail.com>
2024-11-22 12:11:46 +0900

    Replace logo image

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import UnpluginInjectPreload from 'unplugin-inject-preload/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [
     react(),
+    UnpluginInjectPreload({
+      files: [{ entryMatch: /(logo|logo_flat).webp$/ }],
+    }),
   ],
 });
