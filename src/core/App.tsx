67c1d45df9d151685d439c2825de63739fb7ae1a
Hiroyuki Tanjo <expheno@gmail.com>
2024-12-01 14:33:43 +0900

    Install Google Analytics using react-ga4

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,10 +1,21 @@
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useEffect } from 'react';
+import ReactGA from 'react-ga4';
 import SplashScreen from '../ui/SplashScreen';
 import classes from './App.module.scss';
 
 const Screen = lazy(() => import('./Screen'));
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-MPT0YVV9GQ', {
+      gtagOptions: { send_page_view: false },
+    });
+    ReactGA.send({
+      hitType: 'pageview',
+      page: `${window.location.pathname}${window.location.search}`,
+      title: document.title,
+    });
+  }, []);
   return (
     <div className={classes.app}>
       <div className={classes.container}>
