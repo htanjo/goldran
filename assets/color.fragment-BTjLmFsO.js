326a4617a4a3660b374f628ebd9adf9713e2dfc1
Hiroyuki Tanjo <expheno@gmail.com>
2024-12-02 16:34:10 +0900

    Updates

---
+++ assets/color.fragment-BTjLmFsO.js
@@ -0,0 +1,20 @@
+import{S as r}from"./Screen-BLlBCXCq.js";import"./clipPlaneFragment-DxhJv7Pd.js";import"./fogFragmentDeclaration-Cc8Nbcgb.js";import"./index-BSoIr2Zh.js";const e="colorPixelShader",o=`#if defined(VERTEXCOLOR) || defined(INSTANCESCOLOR) && defined(INSTANCES)
+#define VERTEXCOLOR
+varying vec4 vColor;
+#else
+uniform vec4 color;
+#endif
+#include<clipPlaneFragmentDeclaration>
+#include<fogFragmentDeclaration>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) {
+#define CUSTOM_FRAGMENT_MAIN_BEGIN
+#include<clipPlaneFragment>
+#if defined(VERTEXCOLOR) || defined(INSTANCESCOLOR) && defined(INSTANCES)
+gl_FragColor=vColor;
+#else
+gl_FragColor=color;
+#endif
+#include<fogFragment>(color,gl_FragColor)
+#define CUSTOM_FRAGMENT_MAIN_END
+}`;r.ShadersStore[e]=o;const a={name:e,shader:o};export{a as colorPixelShader};
---
+++ assets/extractHighlights.fragment-C3JreXK9.js
@@ -0,0 +1,5 @@
+import{S as o}from"./Screen-BLlBCXCq.js";import"./helperFunctions-CKuQ1OEF.js";import"./index-BSoIr2Zh.js";const r="extractHighlightsPixelShader",e=`#include<helperFunctions>
+varying vec2 vUV;uniform sampler2D textureSampler;uniform float threshold;uniform float exposure;
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) 
+{gl_FragColor=texture2D(textureSampler,vUV);float luma=dot(LuminanceEncodeApprox,gl_FragColor.rgb*exposure);gl_FragColor.rgb=step(threshold,luma)*gl_FragColor.rgb;}`;o.ShadersStore[r]=e;const i={name:r,shader:e};export{i as extractHighlightsPixelShader};
