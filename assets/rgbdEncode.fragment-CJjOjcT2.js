c9ae5d3fcc48e3d9e77adf682b222cd58eb2a64c
Hiroyuki Tanjo <expheno@gmail.com>
2024-12-02 22:26:01 +0900

    Updates

--- assets/rgbdEncode.fragment-CJjOjcT2.js
+++
@@ -1,5 +0,0 @@
-import{S as t}from"./Screen-BLlBCXCq.js";import"./helperFunctions-BQQGLAqu.js";import"./index-BSoIr2Zh.js";const e="rgbdEncodePixelShader",r=`varying vUV: vec2f;var textureSamplerSampler: sampler;var textureSampler: texture_2d<f32>;
-#include<helperFunctions>
-#define CUSTOM_FRAGMENT_DEFINITIONS
-@fragment
-fn main(input: FragmentInputs)->FragmentOutputs {fragmentOutputs.color=toRGBD(textureSample(textureSampler,textureSamplerSampler,input.vUV).rgb);}`;t.ShadersStoreWGSL[e]=r;const p={name:e,shader:r};export{p as rgbdEncodePixelShaderWGSL};
