061f27cfd0bb57ad7eb31c75dd86ec40a29910d9
Hiroyuki Tanjo <expheno@gmail.com>
2025-02-15 23:12:42 +0900

    Update animation

--- src/settings/captions.ts
+++ src/settings/captions.ts
@@ -7,10 +7,10 @@ export interface Caption {
 export const captions: Caption[] = [
   { id: 'incantation', startFrame: 20 * 2.5, type: 'quote' },
   { id: 'vehicleData', startFrame: 80 * 2.5, type: 'info' },
-  { id: 'dranShout', startFrame: 280 * 2.5, type: 'quote' },
-  { id: 'dranData', startFrame: 340 * 2.5, type: 'info' },
-  { id: 'golgonShout', startFrame: 400 * 2.5, type: 'quote' },
-  { id: 'golgonData', startFrame: 500 * 2.5, type: 'info' },
+  { id: 'dranShout', startFrame: 300 * 2.5, type: 'quote' },
+  { id: 'dranData', startFrame: 360 * 2.5, type: 'info' },
+  { id: 'golgonShout', startFrame: 430 * 2.5, type: 'quote' },
+  { id: 'golgonData', startFrame: 540 * 2.5, type: 'info' },
   { id: 'goldranShout', startFrame: 1240 * 2.5, type: 'quote' },
   { id: 'goldranData', startFrame: 1420 * 2.5, type: 'info' },
 ];
--- src/settings/frames.ts
+++ src/settings/frames.ts
@@ -1,4 +1,4 @@
 export const moveSpeed = 0.1; // Number of frames advanced by 1px scroll input.
-export const maxFrame = 1440 * 2.5;
+export const maxFrame = 1500 * 2.5;
 export const autoplaySpeed = 0.75;
 export const lightChangeFrame = 400 * 2.5; // Frame number to change light between former and latter.
