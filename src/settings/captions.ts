3de831fa1870811b4e5471c65a737aa92c498757
Hiroyuki Tanjo <expheno@gmail.com>
2025-02-16 00:07:19 +0900

    Adjust caption timing

--- src/settings/captions.ts
+++ src/settings/captions.ts
@@ -7,9 +7,9 @@ export interface Caption {
 export const captions: Caption[] = [
   { id: 'incantation', startFrame: 20 * 2.5, type: 'quote' },
   { id: 'vehicleData', startFrame: 80 * 2.5, type: 'info' },
-  { id: 'dranShout', startFrame: 300 * 2.5, type: 'quote' },
-  { id: 'dranData', startFrame: 360 * 2.5, type: 'info' },
-  { id: 'golgonShout', startFrame: 430 * 2.5, type: 'quote' },
+  { id: 'dranShout', startFrame: 310 * 2.5, type: 'quote' },
+  { id: 'dranData', startFrame: 380 * 2.5, type: 'info' },
+  { id: 'golgonShout', startFrame: 440 * 2.5, type: 'quote' },
   { id: 'golgonData', startFrame: 540 * 2.5, type: 'info' },
   { id: 'goldranShout', startFrame: 1240 * 2.5, type: 'quote' },
   { id: 'goldranData', startFrame: 1420 * 2.5, type: 'info' },
