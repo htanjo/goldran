cd5c0bb2e43e27521c9e9ffdf857417cb6ba0e62
Hiroyuki Tanjo <expheno@gmail.com>
2025-02-24 16:45:02 +0900

    Adjust caption timing

--- src/settings/captions.ts
+++ src/settings/captions.ts
@@ -10,7 +10,7 @@ export const captions: Caption[] = [
   { id: 'dranDiagram', startFrame: 220 * 2.5, type: 'image' },
   { id: 'dranShout', startFrame: 310 * 2.5, type: 'quote' },
   { id: 'dranData', startFrame: 370 * 2.5, type: 'info' },
-  { id: 'golgonShout', startFrame: 470 * 2.5, type: 'quote' },
+  { id: 'golgonShout', startFrame: 440 * 2.5, type: 'quote' },
   { id: 'golgonData', startFrame: 560 * 2.5, type: 'info' },
   { id: 'combinationData', startFrame: 680 * 2.5, type: 'info' },
   { id: 'goldranDiagram', startFrame: 890 * 2.5, type: 'image' },
