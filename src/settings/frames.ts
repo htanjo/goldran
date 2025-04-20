094667bff0b779144728c8edba55720966168356
Hiroyuki Tanjo <expheno@gmail.com>
2025-04-20 10:38:14 +0900

    Adjust auto animation speed

--- src/settings/frames.ts
+++ src/settings/frames.ts
@@ -1,6 +1,6 @@
 export const moveSpeed = 0.1; // Number of frames advanced by 1px scroll input.
 export const maxFrame = 1500 * 2.5;
-export const autoplaySpeed = 0.75;
+export const autoplaySpeed = 0.65;
 export const rewindSpeed = 30;
 export const vrSpeedRatio = 0.8;
 export const lightChangeFrame = 440 * 2.5; // Frame number to change light between former and latter.
