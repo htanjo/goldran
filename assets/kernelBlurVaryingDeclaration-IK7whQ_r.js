ddaedf6df855ee29710a4f5f882b73cf3bdebfef
Hiroyuki Tanjo <expheno@gmail.com>
2025-05-11 14:38:46 +0900

    Updates

---
+++ assets/kernelBlurVaryingDeclaration-IK7whQ_r.js
@@ -0,0 +1,1 @@
+import{S as r}from"./Screen-CYylH-ma.js";const e="kernelBlurVaryingDeclaration",a="varying sampleCoord{X}: vec2f;";r.IncludesShadersStoreWGSL[e]=a;
--- assets/kernelBlurVaryingDeclaration-ZWLkrcBf.js
+++
@@ -1,1 +0,0 @@
-import{S as r}from"./Screen-BZIixlpT.js";const e="kernelBlurVaryingDeclaration",a="varying vec2 sampleCoord{X};";r.IncludesShadersStore[e]=a;
