68e5db3f41ee2e76fc2f2807b4f725a2515feb11
Hiroyuki Tanjo <expheno@gmail.com>
2025-05-11 16:10:58 +0900

    Updates

---
+++ assets/imageProcessing.fragment-5ifpztH_.js
@@ -0,0 +1,18 @@
+import{S as i}from"./Screen-BY_NNNBY.js";import"./imageProcessingFunctions-cpjEVUru.js";import"./helperFunctions-H6pdpRgq.js";import"./index-DBOmaRJo.js";const e="imageProcessingPixelShader",r=`varying vec2 vUV;uniform sampler2D textureSampler;
+#include<imageProcessingDeclaration>
+#include<helperFunctions>
+#include<imageProcessingFunctions>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void)
+{vec4 result=texture2D(textureSampler,vUV);
+#ifdef IMAGEPROCESSING
+#ifndef FROMLINEARSPACE
+result.rgb=toLinearSpace(result.rgb);
+#endif
+result=applyImageProcessing(result);
+#else
+#ifdef FROMLINEARSPACE
+result=applyImageProcessing(result);
+#endif
+#endif
+gl_FragColor=result;}`;i.ShadersStore[e]=r;const a={name:e,shader:r};export{a as imageProcessingPixelShader};
